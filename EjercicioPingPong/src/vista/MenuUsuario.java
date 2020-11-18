c1c60d88dc1390d59b6f35e04129d66e72e09c05
Pacop <Pacop@DESKTOP-AVEFR0B.home>
2020-11-18 17:10:54 +0100

    Actualización de noticias con scrollbar, falta funcionalidad de botones

--- EjercicioPingPong/src/vista/MenuUsuario.java
+++ EjercicioPingPong/src/vista/MenuUsuario.java
@@ -28,6 +28,15 @@ import java.awt.event.ActionListener;
 import java.awt.event.AdjustmentEvent;
 import java.awt.event.ContainerAdapter;
 import java.awt.event.ContainerEvent;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.swing.JTextArea;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.border.LineBorder;
+import java.awt.Component;
+import java.awt.ComponentOrientation;
+import java.awt.Cursor;
 
 public class MenuUsuario extends JFrame {
 
@@ -39,6 +48,8 @@ public class MenuUsuario extends JFrame {
 	public JButton btnClasificacin;
 	public JButton btnCerrarSesin;
 	private JLabel lblNewLabel_2;
+	private JTextArea Noticia1;
+	private JTextArea Noticia2;
 	
 	/**
 	 * Launch the application.
@@ -71,6 +82,11 @@ public class MenuUsuario extends JFrame {
 		contentPane.setLayout(null);
 		setLocationRelativeTo(null);
 		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(MenuUsuario.class.getResource("/img/BannerOficial2.JPG")));
+		lblNewLabel.setBounds(0, 0, 919, 245);
+		contentPane.add(lblNewLabel);
+		
 		btnNoticias = new JButton("NOTICIAS");
 		btnNoticias.setBounds(0, 243, 142, 39);
 		contentPane.add(btnNoticias);
@@ -95,74 +111,67 @@ public class MenuUsuario extends JFrame {
 		btnCerrarSesin.setBounds(767, 243, 142, 39);
 		contentPane.add(btnCerrarSesin);
 		
-		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon(MenuUsuario.class.getResource("/img/BannerOficial2.JPG")));
-		lblNewLabel.setBounds(0, 0, 919, 245);
-		contentPane.add(lblNewLabel);
-		
-		Label label_1 = new Label("New label");
-		label_1.setBounds(0, 0, 62, 22);
-		contentPane.add(label_1);
-		
-		JTextPane txtpnFabricaDeMedallistas = new JTextPane();
-		txtpnFabricaDeMedallistas.setBackground(new Color(192, 192, 192));
-		txtpnFabricaDeMedallistas.setEditable(false);
-		txtpnFabricaDeMedallistas.setFont(new Font("Sitka Text", Font.BOLD | Font.ITALIC, 12));
-		txtpnFabricaDeMedallistas.setText("Fabrica de medallistas chinos: Guo entrena al ping-pong desde hace dos a\u00F1os y acaba de convertirse en un jugador semiprofesional. Bajo esta nueva condici\u00F3n, a partir de ahora le tocar\u00E1 vivir en la residencia de la escuela de deportes de Shichahai, situada en el centro de Pek\u00EDn, donde entrenar\u00E1 casi a diario con otros chicos de su mismo nivel para cumplir su sue\u00F1o: ser campe\u00F3n del mundo. \r\n\r\naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\r\naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\r\naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\r\naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\r\naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\r\naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-		txtpnFabricaDeMedallistas.setBounds(268, 331, 167, 290);
-		contentPane.add(txtpnFabricaDeMedallistas);
-		
-		JTextPane txtpnTerceraConcentracinJuvenil = new JTextPane();
-
-		txtpnTerceraConcentracinJuvenil.setFont(new Font("Candara", Font.BOLD | Font.ITALIC, 12));
-		txtpnTerceraConcentracinJuvenil.setBackground(Color.LIGHT_GRAY);
-		txtpnTerceraConcentracinJuvenil.setEditable(false);
-		txtpnTerceraConcentracinJuvenil.setText("Tercera concentraci\u00F3n juvenil en el Centro de Tecnificaci\u00F3n Infanta Cristina:\r\n\r\nEl grupo juvenil regresa esta semana al Centro de Tecnificaci\u00F3n Infanta Cristina (Los Alc\u00E1zares, Murcia) para realizar una nueva concentraci\u00F3n de entrenamientos, continuando con la planificaci\u00F3n deportiva, elaborada espec\u00EDficamente para suplir la inactividad de competici\u00F3n internacional.\r\n\r\nDel 16 al 21 de noviembre, los deportistas convocados realizar\u00E1n doble sesi\u00F3n de entrenamientos, que servir\u00E1n para continuar evaluando su progreso. Adem\u00E1s, todos los deportistas y t\u00E9cnicos de la RFETM han pasado una prueba serol\u00F3gica previa a su entrada en la concentraci\u00F3n.");
-		txtpnTerceraConcentracinJuvenil.setBounds(692, 331, 185, 290);
-		contentPane.add(txtpnTerceraConcentracinJuvenil);
-		
 		JButton btnNewButton = new JButton("Leer");
-		btnNewButton.setBounds(632, 599, 89, 23);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				btnNewButton.setText("<HTML>Click the <FONT color=\"#000099\"><U>link</U></FONT>" 
+					     + " to go to the Java website.</HTML>"); 
+			}
+		});
+		btnNewButton.setBounds(509, 587, 89, 23);
 		contentPane.add(btnNewButton);
 		
 		JLabel lblNewLabel_3 = new JLabel("");
-		lblNewLabel_3.setLabelFor(txtpnTerceraConcentracinJuvenil);
 		lblNewLabel_3.setIcon(new ImageIcon(MenuUsuario.class.getResource("/img/noticia2.jpg")));
-		lblNewLabel_3.setBounds(513, 331, 200, 290);
+		lblNewLabel_3.setBounds(509, 297, 184, 313);
 		contentPane.add(lblNewLabel_3);
 		
 		JButton btnNewButton_1 = new JButton("Leer");
-		btnNewButton_1.setBounds(180, 598, 89, 23);
+		btnNewButton_1.setBounds(0, 587, 89, 23);
 		contentPane.add(btnNewButton_1);
 		
 		lblNewLabel_2 = new JLabel("");
 		lblNewLabel_2.setIcon(new ImageIcon(MenuUsuario.class.getResource("/img/noticias1.jpg")));
-		lblNewLabel_2.setBounds(0, 331, 269, 290);
+		lblNewLabel_2.setBounds(0, 297, 224, 313);
 		contentPane.add(lblNewLabel_2);
 		
-		JLabel lblNewLabel_1_1 = new JLabel("ULTIMAS NOTICIAS!!");
-		lblNewLabel_1_1.setForeground(Color.GREEN);
-		lblNewLabel_1_1.setFont(new Font("Comic Sans MS", Font.BOLD, 14));
-		lblNewLabel_1_1.setBounds(608, 276, 167, 37);
-		contentPane.add(lblNewLabel_1_1);
 		
-		JScrollBar scrollBar = new JScrollBar();
-		scrollBar.addAdjustmentListener(new AdjustmentListener() {
-			public void adjustmentValueChanged(AdjustmentEvent arg0) {
-				txtpnFabricaDeMedallistas.setText("Fabrica de medallistas chinos: Guo entrena al ping-pong desde hace dos a\u00F1os y acaba de convertirse en un jugador semiprofesional. Bajo esta nueva condici\u00F3n, a partir de ahora le tocar\u00E1 vivir en la residencia de la escuela de deportes de Shichahai, situada en el centro de Pek\u00EDn, donde entrenar\u00E1 casi a diario con otros chicos de su mismo nivel para cumplir su sue\u00F1o: ser campe\u00F3n del mundo. \r\n\r\naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\r\naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\r\naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\r\naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\r\naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\r\naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-
-			}
-		});
-		scrollBar.setBounds(434, 331, 23, 290);
-		contentPane.add(scrollBar);
+		JScrollPane js = new JScrollPane(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+		js.setToolTipText("left");
+		js.setViewportBorder(new LineBorder(new Color(0, 0, 0), 2));
+		js.setLocation(223, 297);
+		js.setSize(211, 313);
+		js.setVisible(true);
+		contentPane.add(js);
 		
-		JScrollBar scrollBar_1 = new JScrollBar();
-		scrollBar_1.setBounds(882, 331, 17, 290);
-		contentPane.add(scrollBar_1);
-		setLocationRelativeTo(null);
+		Noticia1 = new JTextArea();
+		Noticia1.setWrapStyleWord(true);
+		Noticia1.setLineWrap(true);
+		Noticia1.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+		Noticia1.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		Noticia1.setFont(new Font("Century Gothic", Font.PLAIN, 14));
+		Noticia1.setBackground(Color.LIGHT_GRAY);
+		Noticia1.setEditable(false);
+		Noticia1.setText("F\u00E1brica de medallistas chinos:\r\n\r\nGuo entrena al ping-pong desde hace dos a\u00F1os y acaba de convertirse en un jugador semiprofesional. Bajo esta nueva condici\u00F3n, a partir de ahora le tocar\u00E1 vivir en la residencia de la escuela de deportes de Shichahai, situada en el centro de Pek\u00EDn, donde entrenar\u00E1 casi a diario con otros chicos de su mismo nivel para cumplir su sue\u00F1o: ser campe\u00F3n del mundo. Shichahai es una de las ocho escuelas estatales de formaci\u00F3n de deportistas de \u00E9lite de la capital china. Fundada en 1958, est\u00E1 especializado en ping-pong, boxeo, taekwondo, golf, b\u00E1dminton, esgrima, gimnasia y artes marciales. De sus instalaciones han salido 46 campeones del mundo, siete de ellos oros ol\u00EDmpicos. Sus retratos y logros presiden las salas de entrenamiento, donde decenas de ni\u00F1os y adolescentes se esfuerzan a diario para un d\u00EDa ser como ellos bajo la atenta mirada de los entrenadores.");
+		js.setViewportView(Noticia1);
+		
+		JScrollPane js2 = new JScrollPane(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+		js2.setViewportBorder(new LineBorder(new Color(0, 0, 0), 2));
+		js2.setLocation(700, 297);
+		js2.setSize(209, 313);
+		contentPane.add(js2);
+		
+		Noticia2 = new JTextArea();
+		Noticia2.setWrapStyleWord(true);
+		Noticia2.setLineWrap(true);
+		Noticia2.setFont(new Font("Century Gothic", Font.PLAIN, 14));
+		Noticia2.setEditable(false);
+		Noticia2.setText("Tercera concentraci\u00F3n juvenil en el Centro de Tecnificaci\u00F3n Infanta Cristina\r\n\r\nDel 16 al 21 de noviembre, los deportistas convocados realizar\u00E1n doble sesi\u00F3n de entrenamientos, que servir\u00E1n para continuar evaluando su progreso. Adem\u00E1s, todos los deportistas y t\u00E9cnicos de la RFETM han pasado una prueba serol\u00F3gica previa a su entrada en la concentraci\u00F3n.\r\n\r\nEl grupo juvenil convocado para esta concentraci\u00F3n est\u00E1 formado por los siguientes deportistas: Aimar Chamorro, Aitor Colina, Alberto Pomares, Ander Cepas, Daniel Berzosa, \u00C9ric Cintas, Diego Lillo, H\u00E9ctor Saavedra, Iker Gonz\u00E1lez, Jos\u00E9 Carlos Guillot, Marc Mir\u00F3, Marcos Garc\u00EDa, Mart\u00EDn Reche, Miguel N\u00FA\u00F1ez, Pablo Bobo y Rafael De Las Heras. Como t\u00E9cnicos responsables han acudido Rafa Taboada y Jes\u00FAs Cantero.");
+		Noticia2.setBackground(Color.LIGHT_GRAY);
+		js2.setViewportView(Noticia2);
+		
 		
 		
 	}
-
-	
 }
