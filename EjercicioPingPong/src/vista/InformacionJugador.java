759316ab61cfe9954557309943eb93efe5560c50
Victorse9 <victor.rse97@gmail.com>
2020-11-20 12:52:15 +0100

    Ajuste Informacion Jugador

--- EjercicioPingPong/src/vista/InformacionJugador.java
+++ EjercicioPingPong/src/vista/InformacionJugador.java
@@ -72,40 +72,40 @@ public class InformacionJugador extends JFrame {
 		lblDefensa.setForeground(new Color(34, 139, 34));
 		lblDefensa.setBackground(new Color(34, 139, 34));
 		lblDefensa.setFont(new Font("Rockwell Extra Bold", Font.PLAIN, 15));
-		lblDefensa.setBounds(255, 399, 68, 14);
+		lblDefensa.setBounds(254, 432, 68, 14);
 		contentPane.add(lblDefensa);
 		// Edad
 		lblEdad = new JLabel("");
 		lblEdad.setForeground(new Color(34, 139, 34));
 		lblEdad.setBackground(new Color(34, 139, 34));
 		lblEdad.setFont(new Font("Rockwell Extra Bold", Font.PLAIN, 15));
-		lblEdad.setBounds(255, 143, 98, 14);
+		lblEdad.setBounds(254, 176, 98, 14);
 		contentPane.add(lblEdad);
 
 		JLabel lbl1 = new JLabel("NOMBRE:");
 		lbl1.setForeground(Color.WHITE);
 		lbl1.setFont(new Font("Rockwell Extra Bold", Font.BOLD, 15));
-		lbl1.setBounds(71, 55, 183, 14);
+		lbl1.setBounds(70, 88, 183, 14);
 		contentPane.add(lbl1);
 
 		JLabel lbl2 = new JLabel("ALIAS:");
 		lbl2.setForeground(Color.WHITE);
 		lbl2.setFont(new Font("Rockwell Extra Bold", Font.BOLD, 15));
-		lbl2.setBounds(71, 100, 183, 14);
+		lbl2.setBounds(70, 133, 183, 14);
 		contentPane.add(lbl2);
 
 		JLabel lbl5 = new JLabel("LIGA:");
 		lbl5.setForeground(Color.WHITE);
 		lbl5.setBackground(Color.BLACK);
 		lbl5.setFont(new Font("Rockwell Extra Bold", Font.BOLD, 15));
-		lbl5.setBounds(71, 279, 183, 14);
+		lbl5.setBounds(70, 312, 183, 14);
 		contentPane.add(lbl5);
 
 		btnGrito = new JButton("GRITO");
 		btnGrito.setForeground(new Color(255, 255, 255));
 		btnGrito.setBackground(new Color(34, 139, 34));
 		btnGrito.setFont(new Font("Rockwell Extra Bold", Font.PLAIN, 15));
-		btnGrito.setBounds(481, 429, 352, 39);
+		btnGrito.setBounds(480, 462, 352, 39);
 		contentPane.add(btnGrito);
 
 		btnVolver = new JButton("VOLVER");
@@ -119,37 +119,37 @@ public class InformacionJugador extends JFrame {
 		lbl6.setForeground(Color.WHITE);
 		lbl6.setBackground(Color.BLACK);
 		lbl6.setFont(new Font("Rockwell Extra Bold", Font.BOLD, 15));
-		lbl6.setBounds(71, 230, 174, 19);
+		lbl6.setBounds(70, 263, 174, 19);
 		contentPane.add(lbl6);
 
 		JLabel lbl4 = new JLabel("CIUDAD:");
 		lbl4.setForeground(Color.WHITE);
 		lbl4.setFont(new Font("Rockwell Extra Bold", Font.BOLD, 15));
-		lbl4.setBounds(71, 184, 183, 14);
+		lbl4.setBounds(70, 217, 183, 14);
 		contentPane.add(lbl4);
 
 		JLabel lbl8 = new JLabel("ATAQUE:");
 		lbl8.setForeground(Color.WHITE);
 		lbl8.setFont(new Font("Rockwell Extra Bold", Font.BOLD, 15));
-		lbl8.setBounds(71, 358, 183, 14);
+		lbl8.setBounds(70, 391, 183, 14);
 		contentPane.add(lbl8);
 
 		JLabel lbl9 = new JLabel("DEFENSA:");
 		lbl9.setForeground(Color.WHITE);
 		lbl9.setFont(new Font("Rockwell Extra Bold", Font.BOLD, 15));
-		lbl9.setBounds(71, 394, 183, 19);
+		lbl9.setBounds(70, 427, 183, 19);
 		contentPane.add(lbl9);
 
 		JLabel lbl3 = new JLabel("EDAD:");
 		lbl3.setForeground(Color.WHITE);
 		lbl3.setFont(new Font("Rockwell Extra Bold", Font.BOLD, 15));
-		lbl3.setBounds(71, 143, 183, 14);
+		lbl3.setBounds(70, 176, 183, 14);
 		contentPane.add(lbl3);
 
 		JLabel lbl7 = new JLabel("GOLPE ESTRELLA:");
 		lbl7.setForeground(Color.WHITE);
 		lbl7.setFont(new Font("Rockwell Extra Bold", Font.BOLD, 15));
-		lbl7.setBounds(71, 317, 202, 19);
+		lbl7.setBounds(70, 350, 202, 19);
 		contentPane.add(lbl7);
 
 		// Información de la bbdd
@@ -159,54 +159,54 @@ public class InformacionJugador extends JFrame {
 		lblNombre.setForeground(new Color(34, 139, 34));
 		lblNombre.setBackground(new Color(34, 139, 34));
 		lblNombre.setFont(new Font("Rockwell Extra Bold", Font.PLAIN, 15));
-		lblNombre.setBounds(255, 55, 183, 14);
+		lblNombre.setBounds(254, 88, 183, 14);
 		contentPane.add(lblNombre);
 		// Alias
 		lblAlias = new JLabel("");
 		lblAlias.setForeground(new Color(34, 139, 34));
 		lblAlias.setBackground(new Color(34, 139, 34));
 		lblAlias.setFont(new Font("Rockwell Extra Bold", Font.PLAIN, 15));
-		lblAlias.setBounds(255, 100, 149, 14);
+		lblAlias.setBounds(254, 133, 149, 14);
 		contentPane.add(lblAlias);
 		// Liga
 		lblLiga = new JLabel("");
 		lblLiga.setForeground(new Color(34, 139, 34));
 		lblLiga.setBackground(new Color(34, 139, 34));
 		lblLiga.setFont(new Font("Rockwell Extra Bold", Font.PLAIN, 15));
-		lblLiga.setBounds(255, 279, 149, 14);
+		lblLiga.setBounds(254, 312, 149, 14);
 		contentPane.add(lblLiga);
 		// Lateralidad
 		lblLateralidad = new JLabel("");
 		lblLateralidad.setForeground(new Color(34, 139, 34));
 		lblLateralidad.setBackground(new Color(34, 139, 34));
 		lblLateralidad.setFont(new Font("Rockwell Extra Bold", Font.PLAIN, 15));
-		lblLateralidad.setBounds(255, 235, 149, 14);
+		lblLateralidad.setBounds(254, 268, 149, 14);
 		contentPane.add(lblLateralidad);
 		// Golpe Estrella
 		lblGolpeEstrella = new JLabel("");
 		lblGolpeEstrella.setForeground(new Color(34, 139, 34));
 		lblGolpeEstrella.setBackground(new Color(34, 139, 34));
 		lblGolpeEstrella.setFont(new Font("Rockwell Extra Bold", Font.PLAIN, 15));
-		lblGolpeEstrella.setBounds(255, 320, 191, 14);
+		lblGolpeEstrella.setBounds(254, 353, 191, 14);
 		contentPane.add(lblGolpeEstrella);
 		// Ciudad
 		lblCiudad = new JLabel("");
 		lblCiudad.setForeground(new Color(34, 139, 34));
 		lblCiudad.setBackground(new Color(34, 139, 34));
 		lblCiudad.setFont(new Font("Rockwell Extra Bold", Font.PLAIN, 15));
-		lblCiudad.setBounds(255, 184, 149, 14);
+		lblCiudad.setBounds(254, 217, 149, 14);
 		contentPane.add(lblCiudad);
 		// Ataque
 		lblAtaque = new JLabel("");
 		lblAtaque.setForeground(new Color(34, 139, 34));
 		lblAtaque.setBackground(new Color(34, 139, 34));
 		lblAtaque.setFont(new Font("Rockwell Extra Bold", Font.PLAIN, 15));
-		lblAtaque.setBounds(255, 358, 68, 14);
+		lblAtaque.setBounds(254, 391, 68, 14);
 		contentPane.add(lblAtaque);
 		// Gif
 		lblGif = new JLabel("");
 		lblGif.setIcon(new ImageIcon(InformacionJugador.class.getResource("")));
-		lblGif.setBounds(481, 55, 352, 376);
+		lblGif.setBounds(480, 88, 352, 376);
 		contentPane.add(lblGif);
 
 		lblNewLabel = new JLabel("");
